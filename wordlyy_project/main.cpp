4a35a55b32ca7a4cc79cefba337eee924093fa13
RFHrisova23 <RFHristova23@codingburgas.bg>
2024-11-26 09:25:46 +0100

    Fix comments

---
+++ wordlyy_project/main.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream> 
+#include <string>
+//Change the appearance of the text
+#include <algorithm>
+//Define the functions that manipulate the format of the input and output of the program
+#include <iomanip>
+#include "utils.h"
+#include "icon.h"
+#include "gamemenu.h"
+#include "checkwords.h"
+#include "modedecide.h"
+#include "game.h"
+using namespace std;
+
+
+
+int main() {
+	// Select the level
+	int modeDecide = 0;
+	// Set the number of attempts (by default, there are five attempts)
+	int modeCount = 5; 
+
+	// Make the variable `word` take a value from another function as a random word
+	string word = randomWords();
+
+	// Call the function gamemenu
+	gamemenu(modeDecide, modeCount, word);
+}
